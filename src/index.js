cf1c3f1e889636ac60d4b5db68fff9718b2d4f4c
Sergey Karpynskyi <karpinskiy94@gmail.com>
2021-02-25 16:17:37 +0200

    hw11(color)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
+const startRef = document.querySelector('button[data-action="start"]');
+const stopRef = document.querySelector('button[data-action="stop"]');
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+
+const switchColor = {
+    intervalId: null,
+    isActive: false,
+    start() {
+        if (this.isActive) {
+            return;
+        }
+        this.isActive = true;
+        this.intervalId = setInterval(() => {
+            const numRandom = colors[randomIntegerFromInterval(0, 5)];
+            document.body.style.backgroundColor = numRandom;
+        }, 1000)
+    },
+    stop() {
+        clearInterval(this.intervalId);
+    }
+};
+
+startRef.addEventListener('click', switchColor.start.bind(switchColor))
+stopRef.addEventListener('click', switchColor.stop.bind(switchColor))
+
+    
+  const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+
 
